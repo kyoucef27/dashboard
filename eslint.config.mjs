7d9dd2132eef1073402fb956b30e38002aafd71d
youcef chihab kefif <kefifyoucef2020@gmail.com>
2025-08-19 23:28:09 +0100

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,11 +14,9 @@ const eslintConfig = [
 
   {
     rules: {
-
       "@typescript-eslint/no-explicit-any": "off",
-      " @typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-require-imports": "off",
       "@typescript-eslint/no-unused-vars": "off",
-     
     },
   },
 ];
